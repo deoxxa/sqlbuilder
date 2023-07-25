09248dbec13e721c8e487bc35ba7aff6fe481f84
Conrad Pankoff <deoxxa@fknsrs.biz>
2023-07-25 18:34:57 +1000

    add TypedLiteral for postgres

--- literals.go
+++ literals.go
@@ -15,3 +15,20 @@ func (l *LiteralExpr) AsExpr(s *Serializer) {
 func (e *LiteralExpr) As(alias string) *ColumnAlias {
 	return AliasColumn(e, alias)
 }
+
+func TypedLiteral(typ, text string) *TypedLiteralExpr {
+	return &TypedLiteralExpr{typ: typ, text: text}
+}
+
+type TypedLiteralExpr struct {
+	typ  string
+	text string
+}
+
+func (l *TypedLiteralExpr) AsExpr(s *Serializer) {
+	s.D(l.typ).D(" ").D(l.text)
+}
+
+func (e *TypedLiteralExpr) As(alias string) *ColumnAlias {
+	return AliasColumn(e, alias)
+}
